8ada7f66f59ee19885b9314f110e6a2d8717402b
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2024-11-05 16:36:45 +0700

    fix warn

--- libs/modules/global.module.ts
+++ libs/modules/global.module.ts
@@ -1,25 +1,19 @@
 import {
     Global,
-    MiddlewareConsumer,
     Module,
-    NestModule,
 } from '@nestjs/common'
 import { elasticClientProvider } from '@libs/providers/elastic-client.provider'
 import { envConfigProvider } from '@libs/providers/env.provider'
 import { eventSubjectProvider } from '@libs/providers/event-subject.provider'
-import {
-    RequestContextMiddleware,
-} from '@libs/providers/request-context.provider'
+import { requestContextProvider } from '@libs/providers/request-context.provider'
 import { httpClientProvider } from '@libs/providers/http-client.provider'
 import {
     elasticRepositoryProviders,
     lineRepositoryProvider,
-    ormRepositoryProviders,
 } from '@libs/providers/repository.provider'
-import { requestContextProvider } from '@libs/providers/request-context.provider'
 import { strapiClientProvider } from '@libs/providers/strapi-client.provider'
-import { OrmModule } from '@libs/modules/orm.module'
 import { tokenizationServiceProvider } from '../../apps/auth/src/providers/service.provider'
+
 @Global()
 @Module({
     providers: [

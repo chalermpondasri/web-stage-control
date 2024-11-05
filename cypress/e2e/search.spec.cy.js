a6a06e9f2c06a4627a4153392685f9112261d321
Anupong Knot Chuen-im <anupong@ucconnect.co.th>
2024-11-05 20:21:58 +0700

    refactor: remove duplicate lines in test test: test add ignore to sonar

--- cypress/e2e/search.spec.cy.js
+++ cypress/e2e/search.spec.cy.js
@@ -2,183 +2,234 @@ describe('Search API', () => {
     const baseUrl = 'http://localhost:3003/search'
     // const baseUrl = 'https://billboard-api.dev.ucconnect.co.th/search'
 
-    it('should search track by thai keyword', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/track`,
-            qs: {
-                keyword: 'เกาะ',
-                page: 1,
-                limit: 10,
-            },
-        }).then((response) => {
-            const { data } = response.body
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
-            data.forEach((item) => {
-                expect(item).to.have.property('id')
-                expect(item).to.have.property('name')
-                expect(item).to.have.property('type')
-            })
+    const makeRequest = (method, url, qs = {}) => {
+        return cy.request({
+            method,
+            url: `${baseUrl}${url}`,
+            qs,
+        })
+    }
 
+    const validateResponse = (response, status = 200) => {
+        expect(response.status).to.eq(status)
+        expect(response.body).to.have.property('data')
+    }
+
+    const validateItems = (items, properties) => {
+        items.forEach((item) => {
+            properties.forEach((property) => {
+                expect(item).to.have.property(property)
+            })
+        })
+    }
+
+    it('should search track by thai keyword', () => {
+        makeRequest('GET', '/track', { keyword: 'เกาะ', page: 1, limit: 10 }).then((response) => {
+            validateResponse(response)
+            const { data } = response.body
+            validateItems(data, [
+                'id',
+                'name',
+                'type',
+            ])
             expect(data.length).to.be.greaterThan(0)
             expect(data[0].name).to.include('เกาะ')
         })
     })
 
     it('should search track by english keyword', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/track`,
-            qs: {
-                keyword: 'galaxy',
-                page: 1,
-                limit: 10,
-            },
-        }).then((response) => {
+        makeRequest('GET', '/track', { keyword: 'galaxy', page: 1, limit: 10 }).then((response) => {
+            validateResponse(response)
             const { data } = response.body
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
-            data.forEach((item) => {
-                expect(item).to.have.property('id')
-                expect(item).to.have.property('name')
-                expect(item).to.have.property('type')
-            })
-
+            validateItems(data, [
+                'id',
+                'name',
+                'type',
+            ])
             expect(data.length).to.be.greaterThan(0)
             expect(data[0].name.toLowerCase()).to.include('galaxy')
         })
     })
 
     it('should get top artist', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/artist/top`,
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
-            response.body.data.forEach((artist) => {
-                expect(artist).to.have.property('id')
-                expect(artist).to.have.property('name')
-                expect(artist).to.have.property('type')
-            })
+        makeRequest('GET', '/artist/top').then((response) => {
+            validateResponse(response)
+            validateItems(response.body.data, [
+                'id',
+                'name',
+                'type',
+            ])
         })
     })
 
     it('should get new tracks', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/track/new`,
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
+        makeRequest('GET', '/track/new').then((response) => {
+            validateResponse(response)
             response.body.data.forEach((track) => {
-                expect(track).to.have.property('id')
-                expect(track).to.have.property('name')
-                expect(track).to.have.property('type')
-                expect(track).to.have.property('artists')
+                validateItems(
+                    [
+                        track,
+                    ],
+                    [
+                        'id',
+                        'name',
+                        'type',
+                        'artists',
+                    ],
+                )
                 track.artists.forEach((artist) => {
-                    expect(artist).to.have.property('name')
-                    expect(artist).to.have.property('type')
+                    validateItems(
+                        [
+                            artist,
+                        ],
+                        [
+                            'name',
+                            'type',
+                        ],
+                    )
                 })
                 if (track.album) {
-                    expect(track.album).to.have.property('name')
-                    expect(track.album).to.have.property('type')
+                    validateItems(
+                        [
+                            track.album,
+                        ],
+                        [
+                            'name',
+                            'type',
+                        ],
+                    )
                 }
             })
         })
     })
 
     it('should get top tracks', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/track/top`,
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
+        makeRequest('GET', '/track/top').then((response) => {
+            validateResponse(response)
             response.body.data.forEach((track) => {
-                expect(track).to.have.property('id')
-                expect(track).to.have.property('name')
-                expect(track).to.have.property('type')
-                expect(track).to.have.property('artists')
+                validateItems(
+                    [
+                        track,
+                    ],
+                    [
+                        'id',
+                        'name',
+                        'type',
+                        'artists',
+                    ],
+                )
                 track.artists.forEach((artist) => {
-                    expect(artist).to.have.property('name')
-                    expect(artist).to.have.property('type')
+                    validateItems(
+                        [
+                            artist,
+                        ],
+                        [
+                            'name',
+                            'type',
+                        ],
+                    )
                 })
                 if (track.album) {
-                    expect(track.album).to.have.property('name')
-                    expect(track.album).to.have.property('type')
+                    validateItems(
+                        [
+                            track.album,
+                        ],
+                        [
+                            'name',
+                            'type',
+                        ],
+                    )
                 }
             })
         })
     })
 
     it('should get top albums', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/album/top`,
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
-            response.body.data.forEach((album) => {
-                expect(album).to.have.property('id')
-                expect(album).to.have.property('name')
-                expect(album).to.have.property('type')
-            })
+        makeRequest('GET', '/album/top').then((response) => {
+            validateResponse(response)
+            validateItems(response.body.data, [
+                'id',
+                'name',
+                'type',
+            ])
         })
     })
 
     it('should get album by id', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/album/100`,
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
+        makeRequest('GET', '/album/100').then((response) => {
+            validateResponse(response)
             const album = response.body.data
-            expect(album).to.have.property('id', 100)
-            expect(album).to.have.property('name')
-            expect(album).to.have.property('tracks')
-            expect(album).to.have.property('type')
+            validateItems(
+                [
+                    album,
+                ],
+                [
+                    'id',
+                    'name',
+                    'tracks',
+                    'type',
+                ],
+            )
             album.tracks.forEach((track) => {
-                expect(track).to.have.property('id')
-                expect(track).to.have.property('name')
-                expect(track).to.have.property('artists')
-                expect(track).to.have.property('type')
+                validateItems(
+                    [
+                        track,
+                    ],
+                    [
+                        'id',
+                        'name',
+                        'artists',
+                        'type',
+                    ],
+                )
                 track.artists.forEach((artist) => {
-                    expect(artist).to.have.property('name')
-                    expect(artist).to.have.property('type')
+                    validateItems(
+                        [
+                            artist,
+                        ],
+                        [
+                            'name',
+                            'type',
+                        ],
+                    )
                 })
             })
         })
     })
 
     it('should get artist by id', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/artist/100`,
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('data')
+        makeRequest('GET', '/artist/100').then((response) => {
+            validateResponse(response)
             const artist = response.body.data
-            expect(artist).to.have.property('id', 100)
-            expect(artist).to.have.property('name')
-            expect(artist).to.have.property('tracks')
-            expect(artist).to.have.property('type')
+            validateItems(
+                [
+                    artist,
+                ],
+                [
+                    'id',
+                    'name',
+                    'tracks',
+                    'type',
+                ],
+            )
             artist.tracks.forEach((track) => {
-                expect(track).to.have.property('id')
-                expect(track).to.have.property('name')
-                expect(track).to.have.property('type')
+                validateItems(
+                    [
+                        track,
+                    ],
+                    [
+                        'id',
+                        'name',
+                        'type',
+                    ],
+                )
             })
         })
     })
 
     it('should add hitCounts to track', () => {
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/track/100/hit`,
-        }).then((response) => {
-            expect(response.status).to.eq(200)
+        makeRequest('GET', '/track/100/hit').then((response) => {
+            validateResponse(response)
         })
     })
 })

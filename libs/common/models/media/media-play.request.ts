dced627fefa105e0253ecdb272b6b333bbc92cc0
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2024-11-15 02:18:19 +0700

    what the fix

--- libs/common/models/media/media-play.request.ts
+++ libs/common/models/media/media-play.request.ts
@@ -1,16 +1,18 @@
-import { IsDateString } from 'class-validator'
+import {
+    IsDate,
+} from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
 
 export class MediaPlayRequest {
-    @IsDateString()
+    @IsDate()
     @ApiProperty()
     @Transform(({value}) => new Date(value))
     public timestamp: Date
 }
 
 export class MediaPauseRequest {
-    @IsDateString()
+    @IsDate()
     @ApiProperty()
     @Transform(({value}) => new Date(value))
     public timestamp: Date

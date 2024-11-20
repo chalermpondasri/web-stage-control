c05fb13278dcae7dc436b52d7f05cd8ae19e9f70
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2024-11-20 16:20:10 +0700

    relocate image

--- apps/auth/src/controllers/me.controller.ts
+++ apps/auth/src/controllers/me.controller.ts
@@ -3,9 +3,7 @@ import {
     Controller,
     Get,
     Inject,
-    Param,
     Patch,
-    StreamableFile,
     UseGuards,
 } from '@nestjs/common'
 import { ProviderName } from '@libs/common/constants'
@@ -18,8 +16,6 @@ import {
 } from '@nestjs/swagger'
 import { GenericUserGuard } from '@libs/guards/generic-user.guard'
 import { UserProfileDto } from '@libs/common/models/user/user-profile.dto'
-import { createReadStream } from 'fs'
-import path from 'path'
 import { UpdateProfileRequest } from '@libs/common/models/user/update-profile.request'
 
 @ApiTags(...['user', 'me'])
@@ -55,16 +51,5 @@ export class MeController {
         @Body() request: UpdateProfileRequest,
     ) {
         return this._userService.updateUserProfile(request)
-    }
-
-    @ApiTags(...['user', 'me', 'resource'])
-    @Get('/image/static/:filename')
-    public getUserImage(
-        @Param('filename') filename: string,
-    ) {
-        return new StreamableFile(
-            createReadStream(path.resolve(`./static/${filename}`)),
-            { type: 'image/jpg' },
-        )
     }
 }

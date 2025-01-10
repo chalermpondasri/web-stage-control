648020d0b55644328b1f78ba8ff188535b0fd997
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2025-01-10 17:43:17 +0700

    add swagger doc

--- libs/common/models/user/apple-login.request.ts
+++ libs/common/models/user/apple-login.request.ts
@@ -1,17 +1,26 @@
 import { Type } from 'class-transformer'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class FullName {
+    @ApiProperty()
     public firstName: string
+    @ApiProperty()
     public lastName: string
 }
 export class AppleUserRequest {
+    @ApiProperty({type: FullName})
     @Type(() => FullName)
+
     public name: FullName
+    @ApiProperty()
     public email: string
 }
 export class AppleLoginRequest {
+    @ApiProperty()
     public code: string
+    @ApiProperty()
     public id_token: string
+    @ApiProperty({type: AppleUserRequest})
     @Type(() => AppleUserRequest)
     public user: AppleUserRequest
 
